aa0be91ba8b925544c3e9ef04e1ff33ebfdb2ef4
FauzanBurhanuddin48 <127376615+FauzanBurhanuddin48@users.noreply.github.com>
2023-03-21 03:20:25 +0700

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,14 +1,16 @@
+#Menghitung huruf vokal
 #FauzanBurhanuddin/V3922020
-panjang = int(input('Masukkan Panjang Deret : ')) #digunakkan untuk mengisi inputan berupa bilangan bulat untuk menentukan panjang dari deret bilangan
-fibo = [0, 1] #inisialisasi list 'fibo' dengan  elemen awal 0 dan 1, dimana elemen 0 dan 1 merupakan bilangan awal dari deret bilangan Fibonacci. elemen awal 0 dan 1, dimana elemen 0 dan 1 merupakan bilangan awal dari deret bilangan Fibonacci.
-print(fibo)
+nama = input ("Masukkan Nama Lengkap Anda : ") #Untuk Memasukkan nama dan menyimpan dalam variabel nama
 
-for i in range (2, panjang): #perulangan for yang dimulai dari indeks ke-2 hingga 'panjang -1'. Karena elemen pertama (0) dan kedua (1) telah dimasukkan pada saat inisialisasi.
-    angka1 = fibo[i-2]
-    angka2 = fibo[i-1] #digunakan untuk menyimpan elemen sebelumnya yang dibutuhkan untuk menghasilkan nilai 'nextangka'
-    nextangka = angka1 + angka2 #operasi penjumlahan yang menghasilkan nilai berikutnya pada deret bilangan Fibonacci.
+huruf = len(nama.replace(" ", "")) #menghitung jumlah huruf dalam nama lengkap pengguna. 
+                                   #Fungsi replace() digunakan untuk menghapus spasi dalam nama lengkap, sehingga hanya huruf yang dihitung.
 
-    fibo.append(nextangka) #menambahkan nilai 'nextangka' pada akhir list fibo.
+vokal = "aiueoAIUEO" # String yang berisikan semua huruf vokal
+jumlah_vokal = len([char for char in nama if char in vokal]) # menghitung jumlah huruf vokal dalam nama lengkap pengguna. 
+                                                             #List comprehension digunakan untuk memeriksa setiap karakter dalam nama lengkap dan menghitung karakter yang termasuk dalam string vokal
 
-    print(fibo, end="") #mencetak isi dari list fibo pada setiap perulngan.
-    print() #mencetak baris baru pada setiap perulangan.
+jumlah_konsonan = huruf - jumlah_vokal # menghitung jumlah huruf konsonan dalam nama lengkap dengan mengurangi jumlah huruf vokal dari jumlah huruf total.
+
+print("Jumlah huruf dari nama lengkap Anda adalah :", huruf)
+print("Jumlah huruf vokal dari nama lengkap Anda adalah :", jumlah_vokal)
+print("Jumlah huruf konsonan dari nama lengkap Anda adalah :", jumlah_konsonan)
